44ef36e2bc68e0610297af05a129f5b44f916b1f
Gesang Widigdo <gesangwidigdo@gmail.com>
2024-10-30 17:24:31 +0700

    fix: socket_server accept multiple client

--- socket_server.py
+++ socket_server.py
@@ -1,35 +1,45 @@
 import socket
+import threading
 from tugas1KI_desAlgorithm import encrypt_text, decrypt_text, rkb, rk
 
-def server_program():
-    # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
+clients = []
 
-    server_socket = socket.socket()  # get instance
-    # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
-
-    # configure how many client the server can listen simultaneously
-    server_socket.listen(2)
-    conn, address = server_socket.accept()  # accept new connection
-    print("Connection from: " + str(address))
+def handle_client(conn, address):
+    print("Connection from:", address)
     while True:
-        # receive data stream. it won't accept data packet greater than 1024 bytes
-        data = conn.recv(1024).decode()
-        if not data:
-            # if data is not received break
+        try:
+            data = conn.recv(1024).decode()
+            if not data:
+                break
+
+            decrypted_message = decrypt_text(data, rkb[::-1], rk[::-1])
+            print(f"Message from {address} before decrypt: {data}")
+            print(f"Message from {address}: {decrypted_message}")
+
+            for client in clients:
+                if client != conn:
+                    encrypted_response = encrypt_text(decrypted_message, rkb, rk)
+                    client.send(encrypted_response.encode())
+        except:
             break
 
-        decrypted_message = decrypt_text(data, rkb[::-1], rk[::-1])
-        print('From connected user before decrypt: ' + data)
-        print("From connected user: " + decrypted_message)
-        data = input(' -> ')
-        encrypted_response = encrypt_text(data, rkb, rk)
-        conn.send(encrypted_response.encode())  # send data to the client
+    conn.close()
+    clients.remove(conn)
+    print(f"Connection closed from: {address}")
 
-    conn.close()  # close the connection
+def server_program():
+    host = socket.gethostname()
+    port = 5000
 
+    server_socket = socket.socket()
+    server_socket.bind((host, port))
+    server_socket.listen(2)
 
-if __name__ == '__main__':
+    print("Server is running... Waiting for connections.")
+    while True:
+        conn, address = server_socket.accept()
+        clients.append(conn)
+        threading.Thread(target=handle_client, args=(conn, address)).start()
+
+if _name_ == '_main_':
     server_program()
